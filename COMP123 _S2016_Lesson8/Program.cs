cd1315732d2898595e64c416836d1ca0606484f9
gapillan <gapillan@my.centennialcollege.ca>
2016-07-05 13:23:15 -0400

    refactored the int numerator and denominator

--- COMP123 _S2016_Lesson8/Program.cs
+++ COMP123 _S2016_Lesson8/Program.cs
@@ -10,21 +10,26 @@ namespace COMP123__S2016_Lesson8
     {
         static void Main(string[] args)
         {
-            int numerator = 5;
-            int denominator = 10;
+            Divide();
+        }
+         
+        private static float Divide(int numerator, int denominator) 
+        {
+
+            //int numerator = 5;
+            //int denominator = 10;
 
             try
             {
-                double result = Convert.ToDouble(numerator / denominator);
+                // double result = Convert.ToDouble(numerator) / Convert.ToDouble(denominator); <-- if using double
+                float result = Convert.ToSingle(numerator) / Convert.ToSingle(denominator); // <-- if using float 
                 Console.WriteLine("Result: " + result);
             }
             catch (Exception exception)
             {
-
                 Console.WriteLine("Error: " + exception.Message);
+                return 0f;
             }
-
-            
         }
     }
 }
